98d37fbe4bef5b0b79758da34a72b2cc8f1cb9c4
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2017-10-26 10:45:45 +0200

    Added support for CredentialMap
    Fixes #29

--- src/test/java/nl/esciencecenter/xenon/grpc/MapUtilsTest.java
+++ src/test/java/nl/esciencecenter/xenon/grpc/MapUtilsTest.java
@@ -116,6 +116,185 @@ public class MapUtilsTest {
     }
 
     @Test
+    public void map_Credential_filesystem_map_minimal() {
+        XenonProto.CreateFileSystemRequest request = XenonProto.CreateFileSystemRequest.newBuilder()
+                .setAdaptor("file")
+                .setCredentialMap(XenonProto.CredentialMap.newBuilder()
+                        .build()
+                )
+                .build();
+
+        Credential result = mapCredential(request);
+
+        Credential expected = new CredentialMap();
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void map_Credential_filesystem_map_with_default_as_fallback() {
+        XenonProto.DefaultCredential.Builder fallback = XenonProto.DefaultCredential.newBuilder();
+        XenonProto.CreateSchedulerRequest request = XenonProto.CreateSchedulerRequest.newBuilder()
+                .setAdaptor("file")
+                .setCredentialMap(XenonProto.CredentialMap.newBuilder()
+                        .setDefaultCredential(fallback)
+                        .build()
+                )
+                .build();
+
+        Credential result = mapCredential(request);
+
+        Credential expected = new CredentialMap(new DefaultCredential());
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void map_Credential_filesystem_map_with_usernamedefault_as_fallback() {
+        XenonProto.DefaultCredential.Builder fallback = XenonProto.DefaultCredential.newBuilder()
+                .setUsername("someone");
+        XenonProto.CreateSchedulerRequest request = XenonProto.CreateSchedulerRequest.newBuilder()
+                .setAdaptor("file")
+                .setCredentialMap(XenonProto.CredentialMap.newBuilder()
+                        .setDefaultCredential(fallback)
+                        .build()
+                )
+                .build();
+
+        Credential result = mapCredential(request);
+
+        Credential expected = new CredentialMap(new DefaultCredential("someone"));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void map_Credential_filesystem_map_with_password_as_fallback() {
+        XenonProto.PasswordCredential.Builder fallback = XenonProto.PasswordCredential.newBuilder()
+                .setUsername("someone")
+                .setPassword("mypassword");
+        XenonProto.CreateSchedulerRequest request = XenonProto.CreateSchedulerRequest.newBuilder()
+                .setAdaptor("file")
+                .setCredentialMap(XenonProto.CredentialMap.newBuilder()
+                        .setPasswordCredential(fallback)
+                        .build()
+                )
+                .build();
+
+        Credential result = mapCredential(request);
+
+        Credential expected = new CredentialMap(new PasswordCredential("someone", "mypassword".toCharArray()));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void map_Credential_filesystem_map_with_cert_as_fallback() {
+        XenonProto.CertificateCredential.Builder fallback = XenonProto.CertificateCredential.newBuilder()
+                .setUsername("someone")
+                .setCertfile("/home/someone/.ssh/id_rsa")
+                .setPassphrase("mypassphrase");
+        XenonProto.CreateSchedulerRequest request = XenonProto.CreateSchedulerRequest.newBuilder()
+                .setAdaptor("file")
+                .setCredentialMap(XenonProto.CredentialMap.newBuilder()
+                        .setCertificateCredential(fallback)
+                        .build()
+                )
+                .build();
+
+        Credential result = mapCredential(request);
+
+        Credential expected = new CredentialMap(new CertificateCredential("someone", "/home/someone/.ssh/id_rsa", "mypassphrase".toCharArray()));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void map_Credential_filesystem_map_with_default_entry() {
+        XenonProto.DefaultCredential.Builder fallback = XenonProto.DefaultCredential.newBuilder()
+                .setUsername("someone");
+        XenonProto.UserCredential credEntry = XenonProto.UserCredential.newBuilder()
+                .setDefaultCredential(
+                        XenonProto.DefaultCredential.newBuilder().setUsername("someoneelse").build()
+                ).build();
+        XenonProto.CreateSchedulerRequest request = XenonProto.CreateSchedulerRequest.newBuilder()
+                .setAdaptor("file")
+                .setCredentialMap(XenonProto.CredentialMap.newBuilder()
+                        .setDefaultCredential(fallback)
+                        .putEntries("somehost", credEntry)
+                        .build()
+                )
+                .build();
+
+        Credential result = mapCredential(request);
+
+        CredentialMap expected = new CredentialMap(new DefaultCredential("someone"));
+        expected.put("somehost", new DefaultCredential("someoneelse"));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void map_Credential_filesystem_map_with_userdefault_entry() {
+        XenonProto.UserCredential credEntry = XenonProto.UserCredential.newBuilder()
+                .setDefaultCredential(
+                        XenonProto.DefaultCredential.newBuilder().setUsername("someone").build()
+                ).build();
+        XenonProto.CreateSchedulerRequest request = XenonProto.CreateSchedulerRequest.newBuilder()
+                .setAdaptor("file")
+                .setCredentialMap(XenonProto.CredentialMap.newBuilder()
+                        .putEntries("somehost", credEntry)
+                        .build()
+                )
+                .build();
+
+        Credential result = mapCredential(request);
+
+        CredentialMap expected = new CredentialMap();
+        expected.put("somehost", new DefaultCredential("someone"));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void map_Credential_filesystem_map_with_password_entry() {
+        XenonProto.UserCredential credEntry = XenonProto.UserCredential.newBuilder()
+                .setPasswordCredential(XenonProto.PasswordCredential.newBuilder()
+                        .setUsername("someone")
+                        .setPassword("mypassword")
+                ).build();
+        XenonProto.CreateSchedulerRequest request = XenonProto.CreateSchedulerRequest.newBuilder()
+                .setAdaptor("file")
+                .setCredentialMap(XenonProto.CredentialMap.newBuilder()
+                        .putEntries("somehost", credEntry)
+                        .build()
+                )
+                .build();
+
+        Credential result = mapCredential(request);
+
+        CredentialMap expected = new CredentialMap();
+        expected.put("somehost", new PasswordCredential("someone", "mypassword".toCharArray()));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void map_Credential_filesystem_map_with_cert_entry() {
+        XenonProto.UserCredential credEntry = XenonProto.UserCredential.newBuilder()
+                .setCertificateCredential(XenonProto.CertificateCredential.newBuilder()
+                        .setUsername("someone")
+                        .setCertfile("/home/someone/.ssh/id_rsa")
+                        .setPassphrase("mypassphrase")
+                ).build();
+        XenonProto.CreateSchedulerRequest request = XenonProto.CreateSchedulerRequest.newBuilder()
+                .setAdaptor("file")
+                .setCredentialMap(XenonProto.CredentialMap.newBuilder()
+                        .putEntries("somehost", credEntry)
+                        .build()
+                )
+                .build();
+
+        Credential result = mapCredential(request);
+
+        CredentialMap expected = new CredentialMap();
+        expected.put("somehost", new CertificateCredential("someone", "/home/someone/.ssh/id_rsa", "mypassphrase".toCharArray()));
+        assertEquals(expected, result);
+    }
+
+    @Test
     public void usernameOfCredential_usercredential() {
         UserCredential cred = new DefaultCredential("myusername");
 
